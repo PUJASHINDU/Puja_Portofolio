20e7bfc9632e9ea1ff8d64d100f9ee28bda46e4c
Shindu Astawa <shinduastawa@gmail.com>
2025-08-02 21:33:22 +0800

    Tata letak dan resposive edit

--- src/Components/ServiceComponents.jsx
+++ src/Components/ServiceComponents.jsx
@@ -1,17 +1,17 @@
-import { Button, Card, CardBody, CardFooter, Typography } from "@material-tailwind/react";
+import { Card, CardBody, Typography } from "@material-tailwind/react";
 import serviceone from "../assets/service/serviceone.png";
 import servicetwo from "../assets/service/servicetwo.png";
 import servicetree from "../assets/service/servicetree.png";
 
 export function LogoSection6() {
   return (
-    <section className="py-8 px-8 lg:py-20">
-      <div className="container mx-auto grid gap-8 lg:grid-cols-2 items-center">
+    <section className="py-8 px-4 sm:px-8 lg:py-20">
+      <div className="container mx-auto grid gap-10 lg:grid-cols-2 items-center">
         {/* Bagian Typography */}
-        <div className="text-center lg:text-left -mt-6">
+        <div className="text-center lg:text-left">
           <Typography
             variant="h1"
-            className="-mt-40 mb-5 !text-2xl !leading-snug lg:!text-3xl text-customBlue font-poppins"
+            className="mb-5 !text-2xl !leading-snug lg:!text-3xl text-customBlue font-poppins lg:-mt-80"
           >
             The services I provide
           </Typography>
@@ -25,12 +25,12 @@ export function LogoSection6() {
         </div>
 
         {/* Bagian Card */}
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           {/* Card 1 */}
-          <Card className="w-full h-60 shadow-lg">
-            <img src={servicetree} alt="Website creation" className="w-32 h-32 object-cover" />
-            <CardBody>
-              <Typography variant="h5" color="blue-gray" className="mb-2 -mt-6 font-poppins">
+          <Card className="w-full shadow-lg h-auto py-4">
+            <img src={servicetree} alt="Website creation" className="w-32 h-32 object-cover mx-auto mt-2" />
+            <CardBody className="text-center">
+              <Typography variant="h5" color="blue-gray" className="mb-2 font-poppins">
                 Website Creation
               </Typography>
               <Typography variant="small" className="text-gray-500 font-poppins">
@@ -40,10 +40,10 @@ export function LogoSection6() {
           </Card>
 
           {/* Card 2 */}
-          <Card className="w-full h-60 shadow-lg">
-            <img src={servicetwo} alt="Website Development"  className="w-32 h-32 object-cover"  />
-            <CardBody>
-              <Typography variant="h5" color="blue-gray" className="mb-2 -mt-6 font-poppins">
+          <Card className="w-full shadow-lg h-auto py-4">
+            <img src={servicetwo} alt="Website Development" className="w-32 h-32 object-cover mx-auto mt-2" />
+            <CardBody className="text-center">
+              <Typography variant="h5" color="blue-gray" className="mb-2 font-poppins">
                 Website Development
               </Typography>
               <Typography variant="small" className="text-gray-500 font-poppins">
@@ -53,10 +53,10 @@ export function LogoSection6() {
           </Card>
 
           {/* Card 3 */}
-          <Card className="w-full h-60 shadow-lg">
-            <img src={serviceone} alt="UI Design" className=" h-32 w-32 object-cover" />
-            <CardBody>
-              <Typography variant="h5" color="blue-gray" className="mb-2 -mt-6 font-poppins">
+          <Card className="w-full shadow-lg h-auto py-4 sm:col-span-2">
+            <img src={serviceone} alt="UI Design" className="w-32 h-32 object-cover mx-auto mt-2" />
+            <CardBody className="text-center">
+              <Typography variant="h5" color="blue-gray" className="mb-2 font-poppins">
                 UI Design
               </Typography>
               <Typography variant="small" className="text-gray-500 font-poppins">
