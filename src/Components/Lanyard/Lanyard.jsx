500544e2e5cd05e2e8c09796c9bf6e441643b757
Shindu Astawa <shinduastawa@gmail.com>
2026-06-09 13:59:16 +0800

    new

---
+++ src/Components/Lanyard/Lanyard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import cardGLB from "../../assets/lanyard/card.glb";
+import lanyard from "../../assets/lanyard/lanyard.png";
+const Lanyard = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Lanyard
